37ca5661161893d246645e9358fc8043b8e86b78
chanie-t <tnguyenthuy288@gmail.com>
2025-09-05 17:05:49 +0700

    Update TestUser1.java

--- test/TestUser1.java
+++ test/TestUser1.java
@@ -1,3 +1,16 @@
-public class TestUser1 {
+import HospitalSystem.Model.User;
 
+public class TestUser {
+    public static void main(String[] args) {
+        User u = new User();
+        u.setID(1);
+        u.setFirstName("Nguyen");
+        u.setMiddleName("Van");
+        u.setLastName("A");
+        u.setEmail("a@gmail.com");
+        u.setPhoneNumber("0123456789");
+        u.setPassword("123456");
+
+        System.out.println(u);
+    }
 }
