8d2a6d91a398a7a3b5ffdd42528ca6f99f9b17b7
chanie-t <tnguyenthuy288@gmail.com>
2025-09-05 17:36:17 +0700

    Update TestTime.java

--- test/TestTime.java
+++ test/TestTime.java
@@ -1,5 +1,14 @@
 package test;
 
 public class TestTime {
+    public static void main(String[] args) {
+        // Test lấy thời gian hiện tại
+        String now = Time.getCurrentTime();
+        System.out.println(" Thời gian hiện tại: " + now);
 
+        // Test tính tổng phút
+        int h = 2, m = 30;
+        long totalMinutes = Time.sumMinutes(h, m);
+        System.out.println("thời gian " + h + " giờ " + m + " phút = " + totalMinutes + " phút");
+    }
 }
