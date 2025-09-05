9b1f3ad9d75311e0538d97a73e708bd5cb10c8f7
chanie-t <tnguyenthuy288@gmail.com>
2025-09-05 17:11:51 +0700

    Update Time.java

--- src/Time.java
+++ src/Time.java
@@ -1,3 +1,16 @@
-public class Time {
+package HospitalSystem.Util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time {
+    public static String getCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return now.format(formatter);
+    }
+
+    public static long sumMinutes(int h, int m) {
+        return h * 60 + m;
+    }
 }
